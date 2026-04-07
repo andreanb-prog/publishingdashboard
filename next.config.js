2dbeb5302295e9eee37dfb196293742d83f12c98
andreanb-prog <andreanbonilla@gmail.com>
2026-04-06 16:20:13 -1000

    fix: suppress DEP0169 url.parse() deprecation warning from dependencies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const originalEmitWarning = process.emitWarning.bind(process)
+process.emitWarning = (warning, ...args) => {
+  if (typeof warning === 'string' && warning.includes('DEP0169')) return
+  originalEmitWarning(warning, ...args)
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
